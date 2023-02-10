851b5e3d0a6611268266ca66e713bc565f8db96a
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-10 15:55:55 +0100

    Update Trains Migration data

--- database/migrations/2023_02_10_140145_create_trains_table.php
+++ database/migrations/2023_02_10_140145_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 255);
+            $table->string('Stazione di partenza', 255);
+            $table->string('Stazione di arrivo', 255);
+            $table->dateTime('Orario di partenza')->unsigned();
+            $table->dateTime('Orario di arrivo')->unsigned();
+            $table->string('Codice Treno', 255);
+            $table->tinyInteger('Numero Carrozze', 1)->nullable();
+            $table->tinyInteger('In orario')->unsigned()->default(1);
+            $table->tinyInteger('Cancellato')->unsigned()->default(0);
             $table->timestamps();
         });
     }
